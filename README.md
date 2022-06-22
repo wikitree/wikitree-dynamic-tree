87e4e60eececa29bcbe8ebde7d5c2d403b4219b6
Brian Casey <brian@rootlevelservices.com>
2022-06-22 02:55:11 +0000

    This update includes a number of changes, mostly designed to make the viewer more easily expanded to include new views.
    * Removed the dependency on jquery.cookie.js.
    * Changed the jQuery inclusion (via CDN) to the most recent version.
    * Split the API clientLogin off so that it’s not required, since many views will work fine on public-only profiles.
    * Set up scaffolding for multiple views. The existing dynamic tree is the default. There’s now both an input field to provide a new starting profile ID and a new selection drop-down to select a different view.
    * I created a “dummy/placeholder” view that’s an alternate to the dynamic tree. This just dumps the starting profile data JSON, but its presence should make it clearer how to add more views that can be selected.
    * Added cookie storage of the starting profile id and view, so if you navigate away from the application (e.g. to login to the API), when you return you’ll have the same starting view that you left.
    * Split the JS code for the scaffolding off from that for the specific dynamic-tree view.

--- README.md
+++ README.md
@@ -6,30 +6,41 @@ This app displays a family tree starting from one person profile at WikiTree. Th
 * [WikiTree API](https://github.com/wikitree/wikitree-api)
 * [D3.js](https://d3js.org/)
 * [jQuery](https://jquery.com/)
-* [jquery-cookie](https://github.com/carhartl/jquery-cookie)
 
 ## Usage
 
-The viewing user must be signed into the WikiTree API in order to view their tree. The index.html page follows the example from [API Authentication](https://github.com/wikitree/wikitree-api/blob/main/authentication.md) to do this. 
+The index page sets up a basic control container where the user can provide a starting WikiTree Person ID and select a tree view to use. There is also a button the user can click to sign into the API (required to view content on non-public profiles).
 
-Once signed in, a new tree is created with
+Once there is a starting profile id (either provided in the form or taken from the API login) and a view is selected (defaulting to the WikiTree Dynamic Tree), the view is drawn in a container.
 
-````
- var tree = new TreeViewer('#treeViewerCanvasContainer',viewTreePersonId);
-````
+The dynamic tree can be zoomed and panned with the mouse. Clicking on a plus-sign expands the tree by loading additional ancestors or descendants. Clicking a node displays a pop-up with additional profile information.
 
-where "treeViewerCanvasContainer" is a div on the page where the SVG canvas is built, and viewTreePersonId is set to the Id of the viewed profile.
+A new tree can be displayed by entering a new WikiTree ID in the form and clicking "go". 
+
 
 ## Notes
 
 ### [Tree.js](Tree.js) 
-The "on document ready" code that confirms a sign-in to the WikiTree API, then creates the initial TreeViewer. This file also contains the Tree Viewer code itself, using D3.js for the rendering.
+This is the scaffolding code to set things up on page load and launch the appropriate tree view when a new one is selected or a new starting profile is provided.
+
+Cookies are used to store the API login id (if there is one), the starting profile id, and the selected view. Those are used as defaults when the page reloads.
+
+### [WikiTreeDynamicTree.js](WikiTreeDynamicTree.js)
+This contains the code specific to drawing the WikiTree Dynamic Tree. It uses D3.js for the rendering and code from TreeAPI.js to pull data from the API.
 
 ### [TreeAPI.js](TreeAPI.js)
 Utility functions for getting Person data from the WikiTree API.
 
 ### [Tree.css](Tree.css)
-Style elements for the tree nodes.
+Style elements for the scaffolding and the dynamic-tree nodes.
+
+## New Views
+
+The tree viewer can be extended with additional views. The selection drop-down in index.html should be given a new option. The value of that option should be identified in launchTree() in Tree.js. The appropriate code should then be called to do the rendering (probably in its own JS file).
+
+When building a view, it should be placed in the div with id "treeViewerContainer". In addition the div with id "treeInfo" can be updated with descriptive information about the view.
+
+An example of expanding to a new view is done with a dummy/placeholder view. That simply displays some JSON of person data for the starting profile.
 
 ## Example
 
--- TreeAPI.js
+++ TreeAPI.js
@@ -14,18 +14,18 @@ window.WikiTreeAPI = window.WikiTreeAPI || {};
 // (see getPerson below). The basic fields are just stored in the internal _data array.
 // We pull out the Parent and Child elements as their own Person objects.
 WikiTreeAPI.Person = function(data){
-  this._data = data;
+	this._data = data;
 
-  if(data.Parents){
-    for(var p in data.Parents){
-      this._data.Parents[p] = new WikiTreeAPI.Person(data.Parents[p]);
-    }
-  }
-  if(data.Children){
-    for(var c in data.Children){
-      this._data.Children[c] = new WikiTreeAPI.Person(data.Children[c]);
-    }
-  }
+	if(data.Parents){
+		for(var p in data.Parents){
+			this._data.Parents[p] = new WikiTreeAPI.Person(data.Parents[p]);
+		}
+	}
+	if(data.Children){
+		for(var c in data.Children){
+			this._data.Children[c] = new WikiTreeAPI.Person(data.Children[c]);
+		}
+	}
 };
 
 // Basic "getters" for the data elements.
@@ -41,40 +41,40 @@ WikiTreeAPI.Person.prototype.getFatherId = function() { return this._data.Father
 WikiTreeAPI.Person.prototype.getMotherId = function() { return this._data.Mother; }
 WikiTreeAPI.Person.prototype.getDisplayName = function() { return this._data.BirthName ? this._data.BirthName : this._data.BirthNamePrivate; }
 WikiTreeAPI.Person.prototype.getPhotoUrl = function() {
-  if (this._data.PhotoData && this._data.PhotoData['url']) { 
-    return this._data.PhotoData['url'];
-  }
+	if (this._data.PhotoData && this._data.PhotoData['url']) { 
+		return this._data.PhotoData['url'];
+	}
 }
 // Getters for Mother and Father return the Person objects, if there is one.
 // The getMotherId and getFatherId functions above return the actual .Mother and .Father data elements (ids).
 WikiTreeAPI.Person.prototype.getMother = function() {
-  if (this._data.Mother && this._data.Parents) {
-    return this._data.Parents[this._data.Mother];
-  }
+	if (this._data.Mother && this._data.Parents) {
+		return this._data.Parents[this._data.Mother];
+	}
 };
 WikiTreeAPI.Person.prototype.getFather = function() {
-  if (this._data.Father && this._data.Parents) {
-    return this._data.Parents[this._data.Father];
-  }
+	if (this._data.Father && this._data.Parents) {
+		return this._data.Parents[this._data.Father];
+	}
 };
 
 // We use a few "setters". For the parents, we want to update the Parents Person objects as well as the ids themselves.
 // For TreeViewer we only set the parents and children, so we don't need setters for all the _data elements.
 WikiTreeAPI.Person.prototype.setMother = function(person) {
-  var id = person.getId();
-  var oldId = this._data.Mother;
-  this._data.Mother = id;
-  if (!this._data.Parents) { this._data.Parents = {}; }
-  else if (oldId) { delete this._data.Parents[oldId]; }
-  this._data.Parents[id] = person;
+	var id = person.getId();
+	var oldId = this._data.Mother;
+	this._data.Mother = id;
+	if (!this._data.Parents) { this._data.Parents = {}; }
+	else if (oldId) { delete this._data.Parents[oldId]; }
+	this._data.Parents[id] = person;
 };
 WikiTreeAPI.Person.prototype.setFather = function(person) {
-  var id = person.getId();
-  var oldId = this._data.Father;
-  this._data.Father = id;
-  if (!this._data.Parents) { this._data.Parents = {}; }
-  else if (oldId) { delete this._data.Parents[oldId]; }
-  this._data.Parents[id] = person;
+	var id = person.getId();
+	var oldId = this._data.Father;
+	this._data.Father = id;
+	if (!this._data.Parents) { this._data.Parents = {}; }
+	else if (oldId) { delete this._data.Parents[oldId]; }
+	this._data.Parents[id] = person;
 };
 WikiTreeAPI.Person.prototype.setChildren = function(children) { this._data.Children = children; }
 
@@ -90,15 +90,15 @@ WikiTreeAPI.Person.prototype.setChildren = function(children) { this._data.Child
 // });
 //
 WikiTreeAPI.getPerson = function(id,fields) {
-  return WikiTreeAPI.postToAPI( { 'action': 'getPerson', 'key': id, 'fields': fields.join(','), 'resolveRedirect': 1 } )
-    .then(function(result) {
-      return new WikiTreeAPI.Person(result[0].person);
-    });
+	return WikiTreeAPI.postToAPI( { 'action': 'getPerson', 'key': id, 'fields': fields.join(','), 'resolveRedirect': 1 } )
+		.then(function(result) {
+			return new WikiTreeAPI.Person(result[0].person);
+		});
 }
 
 // This is just a wrapper for the Ajax call, sending along necessary options for the WikiTree API.
 WikiTreeAPI.postToAPI = function(postData) {
-  var API_URL = 'https://api.wikitree.com/api.php';
+	var API_URL = 'https://api.wikitree.com/api.php';
 
 	var ajax = $.ajax({
 		// The WikiTree API endpoint
@@ -110,8 +110,67 @@ WikiTreeAPI.postToAPI = function(postData) {
 		// We're POSTing the data so we don't worry about URL size limits and want JSON back.
 		type: 'POST',
 		dataType: 'json',
-    data: postData
+		data: postData
 	});
 
 	return ajax;
+}
+
+
+// Utility function to get/set cookie data.
+// Adapated from https://github.com/carhartl/jquery-cookie which is obsolete and has been
+// superceded by https://github.com/js-cookie/js-cookie. The latter is a much more complete cookie utility.
+// Here we just want to get and set some simple values in limited circumstances to track an API login.
+// So we'll use a stripped-down function here and eliminate a prerequisite. This function should not be used
+// in complex circumstances.
+// 
+// key     = The name of the cookie to set/read. If reading and no key, then array of all key/value pairs is returned.
+// value   = The value to set the cookie to. If undefined, the value is instead returned. If null, cookie is deleted.
+// options = Used when setting the cookie, 
+//           options.expires = Date or number of days in the future (converted to Date for cookie)
+//           options.path, e.g. "/"
+//           options.domain, e.g. "apps.wikitree.com"
+//           options.secure, if true then cookie created with ";secure"
+WikiTreeAPI.cookie = function(key, value, options) {
+	if (options === undefined) { options = {}; }
+
+	// If we have a value, we're writing/setting the cookie.
+	if (value !== undefined) {
+		if (value === null) { options.expires = -1; }
+		if (typeof options.expires === 'number') {
+			var days = options.expires;
+			options.expires = new Date();
+			options.expires.setDate(options.expires.getDate() + days);
+		}
+		value = String(value);
+		return (document.cookie = [
+			encodeURIComponent(key), '=', value,
+			options.expires ? '; expires=' + options.expires.toUTCString() : '',
+			options.path    ? '; path=' + options.path : '',
+			options.domain  ? '; domain=' + options.domain : '',
+			options.secure  ? '; secure' : ''
+		].join(''));
+	}
+
+	// We're not writing/setting the cookie, we're reading a value from it.
+	var cookies = document.cookie.split('; ');
+
+	var result = key ? null : {};
+	for (var i=0,l=cookies.length; i<l; i++) {
+		var parts = cookies[i].split('=');
+		var name = parts.shift();
+		name = decodeURIComponent(name.replace(/\+/g, ' '));
+		var value = parts.join('=');
+		value = decodeURIComponent(value.replace(/\+/g, ' '));
+
+		if (key && key === name) {
+			result = value;
+			break;
+		}
+		if (!key) {
+			result[name] = value;
+		}
+
+	}
+	return result;
 }
