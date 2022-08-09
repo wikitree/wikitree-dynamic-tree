abad7a2e54e91cb163ed5e3552953b0bfd050333
Brian Casey <brian@rootlevelservices.com>
2022-08-09 16:06:20 +0000

    Start the process of refactoring the code to be more class based rather than prototype based, following the lead from udjeni in a separate pull request off the master branch (where it was mixed with other enhancements).

--- TreeAPI.js
+++ TreeAPI.js
@@ -13,72 +13,74 @@ window.WikiTreeAPI = window.WikiTreeAPI || {};
 // Our basic constructor for a Person. We expect the "person" data from the API returned result
 // (see getPerson below). The basic fields are just stored in the internal _data array.
 // We pull out the Parent and Child elements as their own Person objects.
-WikiTreeAPI.Person = function(data){
-	this._data = data;
-
-	if(data.Parents){
-		for(var p in data.Parents){
-			this._data.Parents[p] = new WikiTreeAPI.Person(data.Parents[p]);
+WikiTreeAPI.Person = class Person {
+	constructor(data) {
+		this._data = data;
+		
+		if(data.Parents){
+			for(var p in data.Parents){
+				this._data.Parents[p] = new WikiTreeAPI.Person(data.Parents[p]);
+			}
+		}
+		if(data.Children){
+			for(var c in data.Children){
+				this._data.Children[c] = new WikiTreeAPI.Person(data.Children[c]);
+			}
 		}
 	}
-	if(data.Children){
-		for(var c in data.Children){
-			this._data.Children[c] = new WikiTreeAPI.Person(data.Children[c]);
+
+	// Basic "getters" for the data elements.
+	getId() { return this._data.Id; }
+	getName() { return this._data.Name; }
+	getGender() { return this._data.Gender; }
+	getBirthDate() { return this._data.BirthDate; }
+	getBirthLocation() { return this._data.BirthLocation; }
+	getDeathDate() { return this._data.DeathDate; }
+	getDeathLocation() { return this._data.DeathLocation; }
+	getChildren() { return this._data.Children; }
+	getFatherId() { return this._data.Father; }
+	getMotherId() { return this._data.Mother; }
+	getDisplayName() { return this._data.BirthName ? this._data.BirthName : this._data.BirthNamePrivate; }
+	getPhotoUrl() {
+		if (this._data.PhotoData && this._data.PhotoData['url']) { 
+			return this._data.PhotoData['url'];
 		}
 	}
-};
 
-// Basic "getters" for the data elements.
-WikiTreeAPI.Person.prototype.getId = function() { return this._data.Id; }
-WikiTreeAPI.Person.prototype.getName = function() { return this._data.Name; }
-WikiTreeAPI.Person.prototype.getGender = function() { return this._data.Gender; }
-WikiTreeAPI.Person.prototype.getBirthDate = function() { return this._data.BirthDate; }
-WikiTreeAPI.Person.prototype.getBirthLocation = function() { return this._data.BirthLocation; }
-WikiTreeAPI.Person.prototype.getDeathDate = function() { return this._data.DeathDate; }
-WikiTreeAPI.Person.prototype.getDeathLocation = function() { return this._data.DeathLocation; }
-WikiTreeAPI.Person.prototype.getChildren = function() { return this._data.Children; }
-WikiTreeAPI.Person.prototype.getFatherId = function() { return this._data.Father; }
-WikiTreeAPI.Person.prototype.getMotherId = function() { return this._data.Mother; }
-WikiTreeAPI.Person.prototype.getDisplayName = function() { return this._data.BirthName ? this._data.BirthName : this._data.BirthNamePrivate; }
-WikiTreeAPI.Person.prototype.getPhotoUrl = function() {
-	if (this._data.PhotoData && this._data.PhotoData['url']) { 
-		return this._data.PhotoData['url'];
-	}
-}
 // Getters for Mother and Father return the Person objects, if there is one.
 // The getMotherId and getFatherId functions above return the actual .Mother and .Father data elements (ids).
-WikiTreeAPI.Person.prototype.getMother = function() {
-	if (this._data.Mother && this._data.Parents) {
-		return this._data.Parents[this._data.Mother];
+	getMother() {
+		if (this._data.Mother && this._data.Parents) {
+			return this._data.Parents[this._data.Mother];
+		}
 	}
-};
-WikiTreeAPI.Person.prototype.getFather = function() {
-	if (this._data.Father && this._data.Parents) {
-		return this._data.Parents[this._data.Father];
+	getFather() {
+		if (this._data.Father && this._data.Parents) {
+			return this._data.Parents[this._data.Father];
+		}
 	}
-};
 
-// We use a few "setters". For the parents, we want to update the Parents Person objects as well as the ids themselves.
-// For TreeViewer we only set the parents and children, so we don't need setters for all the _data elements.
-WikiTreeAPI.Person.prototype.setMother = function(person) {
-	var id = person.getId();
-	var oldId = this._data.Mother;
-	this._data.Mother = id;
-	if (!this._data.Parents) { this._data.Parents = {}; }
-	else if (oldId) { delete this._data.Parents[oldId]; }
-	this._data.Parents[id] = person;
-};
-WikiTreeAPI.Person.prototype.setFather = function(person) {
-	var id = person.getId();
-	var oldId = this._data.Father;
-	this._data.Father = id;
-	if (!this._data.Parents) { this._data.Parents = {}; }
-	else if (oldId) { delete this._data.Parents[oldId]; }
-	this._data.Parents[id] = person;
-};
-WikiTreeAPI.Person.prototype.setChildren = function(children) { this._data.Children = children; }
+	// We use a few "setters". For the parents, we want to update the Parents Person objects as well as the ids themselves.
+	// For TreeViewer we only set the parents and children, so we don't need setters for all the _data elements.
+	setMother(person) {
+		var id = person.getId();
+		var oldId = this._data.Mother;
+		this._data.Mother = id;
+		if (!this._data.Parents) { this._data.Parents = {}; }
+		else if (oldId) { delete this._data.Parents[oldId]; }
+		this._data.Parents[id] = person;
+	}
+	setFather(person) {
+		var id = person.getId();
+		var oldId = this._data.Father;
+		this._data.Father = id;
+		if (!this._data.Parents) { this._data.Parents = {}; }
+		else if (oldId) { delete this._data.Parents[oldId]; }
+		this._data.Parents[id] = person;
+	}
+	setChildren(children) { this._data.Children = children; }
 
-
+} // End Person class definition
 
 // To get a Person for a given id, we POST to the API's getPerson action. When we get a result back,
 // we convert the returned JSON data into a Person object.
@@ -97,7 +99,7 @@ WikiTreeAPI.getPerson = function(id,fields) {
 }
 
 // This is just a wrapper for the Ajax call, sending along necessary options for the WikiTree API.
-WikiTreeAPI.postToAPI = function(postData) {
+WikiTreeAPI.postToAPI = function (postData) {
 	var API_URL = 'https://api.wikitree.com/api.php';
 
 	var ajax = $.ajax({
