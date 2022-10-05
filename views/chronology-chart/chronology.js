ad0632088ba82102e923c854360da8799fe14184
Steve Harris <80127898+harrislineage@users.noreply.github.com>
2022-10-05 08:14:56 -0500

    Update chronology.js

--- views/chronology-chart/chronology.js
+++ views/chronology-chart/chronology.js
@@ -20,87 +20,95 @@ function getChronology(f, selector) {
         list.push({
             x: f.DeathDate.split('-')[0] - f.BirthDate.split('-')[0],
             base: f.BirthDate.split('-')[0],
-            text: f.ShortName
+            text: `<b>${f.ShortName}</b> (${f.BirthDate.split('-')[0]}-${f.DeathDate.split('-')[0]})`
         });
     } else {
         list.push({
             x: new Date().getFullYear() - f.BirthDate.split('-')[0],
             base: f.BirthDate.split('-')[0],
-            text: f.ShortName
+            text: `<b>${f.ShortName}</b> (${f.BirthDate.split('-')[0]}-})`
         });
     }
 
     // Children lines
     var childrenIDs = Object.keys(f.Children);
     for (var i = 0; i < Object.keys(f.Children).length; i++) {
-        if (f.Children[childrenIDs[i]].DeathDate.split('-')[0] != '0000') {
-            if (f.Children[childrenIDs[i]].DeathDate.split('-')[0] - f.Children[childrenIDs[i]].BirthDate.split('-')[0] != 0) {
+        if (f.Children[childrenIDs[i]].DeathDate) {
+            if (f.Children[childrenIDs[i]].DeathDate.split('-')[0] != '0000') {
+                if (f.Children[childrenIDs[i]].DeathDate.split('-')[0] - f.Children[childrenIDs[i]].BirthDate.split('-')[0] != 0) {
+                    list.push({
+                        x: f.Children[childrenIDs[i]].DeathDate.split('-')[0] - f.Children[childrenIDs[i]].BirthDate.split('-')[0],
+                        base: f.Children[childrenIDs[i]].BirthDate.split('-')[0],
+                        text: f.Children[childrenIDs[i]].ShortName
+                    });
+                }
+            } else {
                 list.push({
-                    x: f.Children[childrenIDs[i]].DeathDate.split('-')[0] - f.Children[childrenIDs[i]].BirthDate.split('-')[0],
+                    x: new Date().getFullYear() - f.Children[childrenIDs[i]].BirthDate.split('-')[0],
                     base: f.Children[childrenIDs[i]].BirthDate.split('-')[0],
                     text: f.Children[childrenIDs[i]].ShortName
                 });
             }
-        } else {
-            list.push({
-                x: new Date().getFullYear() - f.Children[childrenIDs[i]].BirthDate.split('-')[0],
-                base: f.Children[childrenIDs[i]].BirthDate.split('-')[0],
-                text: f.Children[childrenIDs[i]].ShortName
-            });
         }
     }
 
     // Parent lines
     var parentIDs = Object.keys(f.Parents);
     for (var i = 0; i < Object.keys(f.Parents).length; i++) {
-        if (f.Parents[parentIDs[i]].DeathDate.split('-')[0] != '0000') {
-            list.push({
-                x: f.Parents[parentIDs[i]].DeathDate.split('-')[0] - f.Parents[parentIDs[i]].BirthDate.split('-')[0],
-                base: f.Parents[parentIDs[i]].BirthDate.split('-')[0],
-                text: f.Parents[parentIDs[i]].ShortName
-            });
-        } else {
-            list.push({
-                x: new Date().getFullYear() - f.Parents[parentIDs[i]].BirthDate.split('-')[0],
-                base: f.Parents[parentIDs[i]].BirthDate.split('-')[0],
-                text: f.Parents[parentIDs[i]].ShortName
-            });
+        if (f.Parents[parentIDs[i]].DeathDate) {
+            if (f.Parents[parentIDs[i]].DeathDate.split('-')[0] != '0000') {
+                list.push({
+                    x: f.Parents[parentIDs[i]].DeathDate.split('-')[0] - f.Parents[parentIDs[i]].BirthDate.split('-')[0],
+                    base: f.Parents[parentIDs[i]].BirthDate.split('-')[0],
+                    text: f.Parents[parentIDs[i]].ShortName
+                });
+            } else {
+                list.push({
+                    x: new Date().getFullYear() - f.Parents[parentIDs[i]].BirthDate.split('-')[0],
+                    base: f.Parents[parentIDs[i]].BirthDate.split('-')[0],
+                    text: f.Parents[parentIDs[i]].ShortName
+                });
+            }
         }
     }
 
     // Spouse lines
     var spouseIDs = Object.keys(f.Spouses);
     for (var i = 0; i < Object.keys(f.Spouses).length; i++) {
-        if (f.Spouses[spouseIDs[i]].DeathDate.split('-')[0] != '0000') {
-            list.push({
-                x: f.Spouses[spouseIDs[i]].DeathDate.split('-')[0] - f.Spouses[spouseIDs[i]].BirthDate.split('-')[0],
-                base: f.Spouses[spouseIDs[i]].BirthDate.split('-')[0],
-                text: f.Spouses[spouseIDs[i]].ShortName
-            });
-        } else {
-            list.push({
-                x: new Date().getFullYear() - f.Spouses[spouseIDs[i]].BirthDate.split('-')[0],
-                base: f.Spouses[spouseIDs[i]].BirthDate.split('-')[0],
-                text: f.Spouses[spouseIDs[i]].ShortName
-            });
+        if (f.Spouses[spouseIDs[i]].DeathDate) {
+            if (f.Spouses[spouseIDs[i]].DeathDate.split('-')[0] != '0000') {
+                list.push({
+                    x: f.Spouses[spouseIDs[i]].DeathDate.split('-')[0] - f.Spouses[spouseIDs[i]].BirthDate.split('-')[0],
+                    base: f.Spouses[spouseIDs[i]].BirthDate.split('-')[0],
+                    text: f.Spouses[spouseIDs[i]].ShortName
+                });
+            } else {
+                list.push({
+                    x: new Date().getFullYear() - f.Spouses[spouseIDs[i]].BirthDate.split('-')[0],
+                    base: f.Spouses[spouseIDs[i]].BirthDate.split('-')[0],
+                    text: f.Spouses[spouseIDs[i]].ShortName
+                });
+            }
         }
     }
 
     // Spouse lines
     var siblingIDs = Object.keys(f.Siblings);
     for (var i = 0; i < Object.keys(f.Siblings).length; i++) {
-        if (f.Siblings[siblingIDs[i]].DeathDate.split('-')[0] != '0000') {
-            list.push({
-                x: f.Siblings[siblingIDs[i]].DeathDate.split('-')[0] - f.Siblings[siblingIDs[i]].BirthDate.split('-')[0],
-                base: f.Siblings[siblingIDs[i]].BirthDate.split('-')[0],
-                text: f.Siblings[siblingIDs[i]].ShortName
-            });
-        } else {
-            list.push({
-                x: new Date().getFullYear() - f.Siblings[siblingIDs[i]].BirthDate.split('-')[0],
-                base: f.Siblings[siblingIDs[i]].BirthDate.split('-')[0],
-                text: f.Siblings[siblingIDs[i]].ShortName
-            });
+        if (f.Siblings[siblingIDs[i]].DeathDate) {
+            if (f.Siblings[siblingIDs[i]].DeathDate.split('-')[0] != '0000') {
+                list.push({
+                    x: f.Siblings[siblingIDs[i]].DeathDate.split('-')[0] - f.Siblings[siblingIDs[i]].BirthDate.split('-')[0],
+                    base: f.Siblings[siblingIDs[i]].BirthDate.split('-')[0],
+                    text: f.Siblings[siblingIDs[i]].ShortName
+                });
+            } else {
+                list.push({
+                    x: new Date().getFullYear() - f.Siblings[siblingIDs[i]].BirthDate.split('-')[0],
+                    base: f.Siblings[siblingIDs[i]].BirthDate.split('-')[0],
+                    text: f.Siblings[siblingIDs[i]].ShortName
+                });
+            }
         }
     }
 
@@ -118,15 +126,15 @@ function getChronology(f, selector) {
     }
     var chrono = [trace]; // allows us to add additional traces
 
-    Plotly.newPlot(selector, chrono,
-        {
-            yaxis: {
-                autorange: 'reversed',
-                showticklabels: false
-            }
-        },
-        {
-            displayModeBar: false
+    var options = {
+        showlegend: false,
+        yaxis: {
+            autorange: 'reversed',
+            showticklabels: false
         }
-    )
+    };
+
+    var config = { displayModeBar: true, displaylogo: false, modeBarButtonsToRemove: ['toggleSpikelines', 'hoverClosestCartesian', 'hoverCompareCartesian', 'toggleHover', 'lasso2d', 'select2d'] }
+
+    Plotly.newPlot(selector, chrono, options, config);
 }
