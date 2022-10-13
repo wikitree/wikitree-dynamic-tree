6eedb08ea35e40426096c637d502c1b0052e9143
Steve Harris <steve@harrislineage.com>
2022-10-13 11:40:19 -0500

    Added Groups, Navigation; Updated Content Labels
    Added grouping of generations (Parents, Siblings, Spouses, Children) and updated the content labels. Navigation can be done by selecting a profile form the timeline. Other display configurations.

--- views/timeline/timeline.js
+++ views/timeline/timeline.js
@@ -1,10 +1,9 @@
 window.TimelineView = class TimelineView extends View {
     meta() {
         return {
-            title: "Timeline",
+            title: "Family Timeline",
             description: `The Timeline view is an interactive visualization chart that allows zooming and panning through
-                a family's chronology. The chronology may show a mixture of markers (single data points) and timespans
-                (starting and ending rows) based on the availability of dates.`,
+                a family chronology. Click on a person to create a new timeline.`,
             docs: "",
         };
     }
@@ -15,48 +14,52 @@ window.TimelineView = class TimelineView extends View {
             key: person_id,
             fields: "Privacy,IsLiving,BirthDateDecade,DeathDateDecade,Parents,Siblings,Derived.ShortName,FirstName,LastNameAtBirth,LastNameCurrent,Name,BirthDate,DeathDate,Children,Spouses,Father,Mother",
         }).then(function (data) {
+            // Let's hide our container to start because grouping causes strange animations
+            $("#view-container").css("width", "0%");
             var focus = data[0].person;
             var container = document.getElementById(selector.replace("#", ""));
             var list = [];
+            var groups = [
+                { id: 1, content: "&#8203;" },
+                { id: 2, content: "&#8203;" },
+                { id: 3, content: "&#8203;" },
+                { id: 4, content: "&#8203;" },
+            ];
 
             /* Here we will call each person to process.
              * We start with our focus profile, and move on to Parents,
              * Siblings, Spouses, and then Children if available.
              */
-            buildEvents(focus);
-
+            buildEvents(focus, "", 2);
             if (focus.Parents) {
                 var parentIDs = Object.keys(focus.Parents);
                 for (var i = 0; i < Object.keys(focus.Parents).length; i++) {
                     if (focus.Parents[parentIDs[i]].BirthDate) {
-                        buildEvents(focus.Parents[parentIDs[i]]);
+                        buildEvents(focus.Parents[parentIDs[i]], "Parent", 1);
                     }
                 }
             }
-
             if (focus.Siblings) {
                 var siblingIDs = Object.keys(focus.Siblings);
                 for (var i = 0; i < Object.keys(focus.Siblings).length; i++) {
                     if (focus.Siblings[siblingIDs[i]].BirthDate) {
-                        buildEvents(focus.Siblings[siblingIDs[i]]);
+                        buildEvents(focus.Siblings[siblingIDs[i]], "Sibling", 2);
                     }
                 }
             }
-
             if (focus.Spouses) {
                 var spouseIDs = Object.keys(focus.Spouses);
                 for (var i = 0; i < Object.keys(focus.Spouses).length; i++) {
                     if (focus.Spouses[spouseIDs[i]].BirthDate) {
-                        buildEvents(focus.Spouses[spouseIDs[i]]);
+                        buildEvents(focus.Spouses[spouseIDs[i]], "Spouse", 3);
                     }
                 }
             }
-
             if (focus.Children) {
                 var childIDs = Object.keys(focus.Children);
                 for (var i = 0; i < Object.keys(focus.Children).length; i++) {
                     if (focus.Children[childIDs[i]].BirthDate) {
-                        buildEvents(focus.Children[childIDs[i]]);
+                        buildEvents(focus.Children[childIDs[i]], "Child", 4);
                     }
                 }
             }
@@ -65,8 +68,15 @@ window.TimelineView = class TimelineView extends View {
              * timespans (both start and end). For each person passed to it, we evaluate
              * the dates, build our content labels, and push to the timeline list.
              */
-            function buildEvents(x) {
-                console.log(x);
+            function buildEvents(x, y, z) {
+                if (y == "") {
+                    var relation = "";
+                    var color = "green";
+                } else {
+                    var relation = `${y}: `;
+                    var color = "blue";
+                }
+
                 if (x.Privacy >= 50) {
                     // Person is at least Public. Dates are shown.
                     if (x.IsLiving == 0) {
@@ -88,8 +98,10 @@ window.TimelineView = class TimelineView extends View {
                             }
                             list.push({
                                 id: x.Name,
-                                content: `${x.ShortName} (b. ${birth.split("-")[0]}; d. unknown)`,
+                                content: `${relation}${x.ShortName} (b. ${birth.split("-")[0]}; d. unknown)`,
                                 start: `${birth}`,
+                                className: color,
+                                group: z,
                             });
                         } else if (x.BirthDate == "0000-00-00" && x.DeathDate != "0000-00-00") {
                             // Birth is unknown, Death is known - marker only
@@ -105,8 +117,10 @@ window.TimelineView = class TimelineView extends View {
                             }
                             list.push({
                                 id: x.Name,
-                                content: `${x.ShortName} (b. unknown; d. ${death.split("-")[0]})`,
+                                content: `${relation}${x.ShortName} (b. unknown; d. ${death.split("-")[0]})`,
                                 start: `${death}`,
+                                className: "red",
+                                group: z,
                             });
                         } else {
                             if (x.BirthDate.split("-")[1] == "00") {
@@ -114,7 +128,7 @@ window.TimelineView = class TimelineView extends View {
                                 var birth = `${x.BirthDate.split("-")[0]}-01-01`;
                             } else if (x.DeathDate.split("-")[2] == "00") {
                                 // Death Day is not known
-                                var birth = `${x.BirthDate.split("-")[0]}-${x.BirthDate.split("-")[0]}-01`;
+                                var birth = `${x.BirthDate.split("-")[0]}-${x.BirthDate.split("-")[1]}-01`;
                             } else {
                                 //  Death Year, Month, and Day are known
                                 var birth = x.BirthDate;
@@ -124,7 +138,7 @@ window.TimelineView = class TimelineView extends View {
                                 var death = `${x.DeathDate.split("-")[0]}-01-01`;
                             } else if (x.DeathDate.split("-")[2] == "00") {
                                 // Death Day is not known
-                                var death = `${x.DeathDate.split("-")[0]}-${x.DeathDate.split("-")[0]}-01`;
+                                var death = `${x.DeathDate.split("-")[0]}-${x.DeathDate.split("-")[1]}-01`;
                             } else {
                                 //  Death Year, Month, and Day are known
                                 var death = x.DeathDate;
@@ -132,15 +146,23 @@ window.TimelineView = class TimelineView extends View {
                             if (birth == death) {
                                 list.push({
                                     id: x.Name,
-                                    content: `${x.ShortName} (b. ${birth.split("-")[0]}; d. ${death.split("-")[0]})`,
+                                    content: `${relation}${x.ShortName} (b. ${birth.split("-")[0]}; d. ${
+                                        death.split("-")[0]
+                                    })`,
                                     start: birth,
+                                    className: "red",
+                                    group: z,
                                 });
                             } else {
                                 list.push({
                                     id: x.Name,
-                                    content: `${x.ShortName} (b. ${birth.split("-")[0]}; d. ${death.split("-")[0]})`,
+                                    content: `${relation}${x.ShortName} (b. ${birth.split("-")[0]}; d. ${
+                                        death.split("-")[0]
+                                    })`,
                                     start: birth,
                                     end: death,
+                                    className: color,
+                                    group: z,
                                 });
                             }
                         }
@@ -153,9 +175,11 @@ window.TimelineView = class TimelineView extends View {
                         var birth = x.BirthDate;
                         list.push({
                             id: x.Name,
-                            content: `${x.ShortName} (b. ${birth})`,
+                            content: `${relation}${x.ShortName} (b. ${birth})`,
                             start: birth,
                             end: death,
+                            className: color,
+                            group: z,
                         });
                     }
                 } else {
@@ -170,25 +194,33 @@ window.TimelineView = class TimelineView extends View {
                             var birth = parseInt(x.BirthDateDecade.replace("s"));
                             list.push({
                                 id: x.Name,
-                                content: `${x.ShortName} (b. ${birth}; d. unknown)`,
+                                content: `${relation}${x.ShortName} (b. ${birth}; d. unknown)`,
                                 start: `${birth}-01-01`,
+                                className: "red",
+                                group: z,
                             });
                         } else if (x.BirthDateDecade == "unknown" && x.DeathDateDecade != "unknown") {
                             // Birth is unknown, Death is known
                             var death = parseInt(x.DeathDateDecade.replace("s"));
                             list.push({
                                 id: x.Name,
-                                content: `${x.ShortName} (b. unknown; d. ${death})`,
+                                content: `${relation}${x.ShortName} (b. unknown; d. ${death})`,
                                 start: `${death}-01-01`,
+                                className: "red",
+                                group: z,
                             });
                         } else {
                             var birth = x.BirthDate;
                             var death = x.DeathDate;
                             list.push({
                                 id: x.Name,
-                                content: `${x.ShortName} (b. ${birth.split("-")[0]}; d. ${death.split("-")[0]})`,
+                                content: `${relation}${x.ShortName} (b. ${birth.split("-")[0]}; d. ${
+                                    death.split("-")[0]
+                                })`,
                                 start: birth,
                                 end: death,
+                                className: color,
+                                group: z,
                             });
                         }
                     } else {
@@ -200,9 +232,11 @@ window.TimelineView = class TimelineView extends View {
                         var birth = x.BirthDate;
                         list.push({
                             id: x.Name,
-                            content: `${x.ShortName} (b. ${birth.split("-")[0]})`,
+                            content: `${relation}${x.ShortName} (b. ${birth.split("-")[0]})`,
                             start: birth,
                             end: death,
+                            className: color,
+                            group: z,
                         });
                     }
                 }
@@ -216,9 +250,20 @@ window.TimelineView = class TimelineView extends View {
                     return b.start - a.start;
                 },
                 zoomMin: 986399999,
+                stack: true,
             };
             // Create the Timeline
-            var timeline = new vis.Timeline(container, items, options);
+            var timeline = new vis.Timeline(container, items, groups, options);
+            // Once the timeline is drawn and items grouped, we can reshow our container
+            timeline.on("currentTimeTick", function () {
+                $("#view-container").css("width", "99.9%");
+            });
+            timeline.on("select", function (properties) {
+                if (properties.items[0] != undefined) {
+                    $("#wt-id-text").val(properties.items[0]);
+                    $("#show-btn").click();
+                }
+            });
         });
     }
 };
