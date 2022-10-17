4bc26a7deabc257807b31a5791f81668c7d4291c
Michal Vašut <michal.vasut@firma.seznam.cz>
2022-10-18 00:52:47 +0200

    Printer Friendly View

---
+++ views/printerFriendly/PrinterFriendlyView.js
@@ -0,0 +1,112 @@
+window.PrinterFriendlyView = class PrinterFriendlyView extends View {
+    WT_DOMAIN = "https://www.wikitree.com";
+    PERSON_FIELDS = ["Id", "Name", "Father", "Mother", "Gender", "Photo"];
+
+    constructor(wtAPI, generationsCount) {
+        super();
+
+        this.wtAPI = wtAPI;
+        this.generationsCount = generationsCount > 0 ? generationsCount : 1;
+    }
+
+    meta() {
+        return {
+            title: "Printer Friendly",
+            description: "",
+            docs: "",
+        };
+    }
+
+    async init(containerSelector, personID) {
+        this.template = Array(this.generationsCount).fill([]);
+
+        let data = await this.wtAPI.getAncestors(personID, this.generationsCount - 1, this.PERSON_FIELDS);
+        this.people = Object.assign({}, ...data.map((x) => ({ [x.Id.toString()]: x })));
+
+        this.prepareTemplate(this.generationsCount, "O"); // O = origin
+
+        // DNA here is combination of O, F, M letters, //e.g. OF - father, OFM - father's side grandma
+        this.assignDNAToPeople(personID, "O");
+
+        this.unknownDNA = Array.from(new Set(this.template.flat())).filter(
+            (dna) =>
+                !Object.values(this.people)
+                    .map((person) => person.dna)
+                    .includes(dna)
+        );
+
+        this.render(containerSelector);
+
+        console.log(data);
+    }
+
+    prepareTemplate(remainingGens, dna) {
+        const currGen = this.generationsCount - remainingGens;
+
+        if (this.generationsCount === currGen) return;
+
+        this.template[currGen] = this.template[currGen].concat(Array(2 ** (remainingGens - 1)).fill(`${dna}`));
+
+        if (remainingGens === 0) return;
+
+        this.prepareTemplate(remainingGens - 1, `${dna}M`); // M = mother
+        this.prepareTemplate(remainingGens - 1, `${dna}F`); // F = father
+    }
+
+    assignDNAToPeople(personID, dna) {
+        if (!this.people[personID]) return;
+        this.people[personID]["dna"] = dna;
+
+        if (this.people[personID].Mother) this.assignDNAToPeople(this.people[personID].Mother, `${dna}M`);
+        if (this.people[personID].Father) this.assignDNAToPeople(this.people[personID].Father, `${dna}F`);
+    }
+
+    render(containerSelector) {
+        let profiles = Object.values(this.people)
+            .map((person) => this.renderPerson(person))
+            .join("");
+
+        let unknownProfiles = this.unknownDNA.map((dna) => this.renderUnknownDNA(dna)).join("");
+
+        document.querySelector(containerSelector).innerHTML = `<div id="profiles">${profiles}${unknownProfiles}</div>`;
+
+        profiles = document.querySelector(`${containerSelector} #profiles`);
+
+        profiles.style.gridTemplateAreas = this.template[0]
+            // switches rows and columns and serializes template to string to be used for css grid
+            .map((_, colIndex) => '"' + this.template.map((row) => row[colIndex]).join(" ") + '"')
+            .join("\n");
+    }
+
+    getDNARelationship(dna) {
+        if (dna.length === 1) {
+            return Object({ M: "Mother", F: "Father" })[dna];
+        } else if (dna.length === 2) {
+            return `Grand${this.getDNARelationship(dna.slice(1)).toLowerCase()}`;
+        } else if (dna.length > 2) {
+            return `G-${this.getDNARelationship(dna.slice(1))}`;
+        }
+    }
+
+    renderUnknownDNA(dna) {
+        return `
+            <div style="grid-area: ${dna};" class="unknown-relative">
+                <p>(${this.getDNARelationship(dna.slice(1))})</p>
+            </div>`;
+    }
+
+    renderPerson(person) {
+        if (!person.dna || person.dna.length > this.generationsCount) return "";
+
+        const photoUrl = person.PhotoData ? `${this.WT_DOMAIN}/${person.PhotoData.url}` : "";
+        const photo = person.dna.length <= 3 ? `<img src="${photoUrl}" class="photo">` : "";
+
+        return `
+            <div style="grid-area: ${person.dna};" class="known-relative">
+                ${photo}
+                <div>
+                    <h2>${person.Name}</h2>
+                </div>
+            </div>`;
+    }
+};
