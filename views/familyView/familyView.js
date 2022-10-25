fe52e46294efa8f42a713fc9acf22902e23e5f16
Geoff Riley <geoffr@adaso.com>
2022-10-25 22:16:34 +0100

    Lots of initial documentation comments (not exciting) plus...
    Pull out of url related strings to allow simpler updating if the need arises.

--- views/familyView/familyView.js
+++ views/familyView/familyView.js
@@ -1,4 +1,14 @@
+/**
+ * Encapsulate a family group sheet within a pair of objects
+ *
+ * @type {Window.FamilyView}
+ */
 window.FamilyView = class FamilyView extends View {
+    /**
+     * Provide information about the view to display in the menu system
+     *
+     * @returns {{docs: string, description: string, title: string}}
+     */
     meta() {
         return {
             title: "Family Group View",
@@ -7,13 +17,66 @@ window.FamilyView = class FamilyView extends View {
         };
     }
 
+    /**
+     * Initialise the view to operate within the given container using the person_id
+     *
+     * @param container_selector
+     * @param person_id
+     */
     init(container_selector, person_id) {
         const view = new FamilyGroup(container_selector, person_id);
         view.displayFamilyGroup();
     }
 };
 
+/**
+ * The working heart of the Family Group Sheet display
+ *
+ * @type {Window.FamilyGroup}
+ */
 window.FamilyGroup = class FamilyGroup {
+    /**
+     * Set of predefined strings to enable alterations if necessary
+     * @returns {string}
+     */
+    get baseWikiURL() {
+        return 'https://www.wikitree.com';
+    }
+
+    get editFamilyURL() {
+        return '/index.php?title=Special:EditFamily';
+    }
+
+    get urlTreeAndTools() {
+        return '/genealogy/';
+    }
+
+    get urlPrivacy() {
+        return '/wiki/Privacy';
+    }
+
+    get urlTrustedList() {
+        return '/wiki/Trusted_List';
+    }
+
+    get imgMaleShadow() {
+        return '/images/icons/male.gif.pagespeed.ce.sk2cBn-ts3.gif';
+    }
+
+    get imgFemaleShadow() {
+        return '/images/icons/female.gif.pagespeed.ce._HpxLyYvZO.gif';
+    }
+
+    get imgFamilyGroup() {
+        return '/images/icons/family-group.gif.pagespeed.ce.xRjS57QXMl.gif';
+    }
+
+    /**
+     * Constructor receives the container_selector and the person_id from the FamilyView that creates it
+     *
+     * @param container_selector
+     * @param person_id
+     */
     constructor(container_selector, person_id) {
         this.selector = container_selector;
         this.person_id = person_id;
@@ -45,12 +108,12 @@ window.FamilyGroup = class FamilyGroup {
     /**
      * Check if a field exists within an object and return it, otherwise return an empty string
      * @param {Object} obj
-     * @param {string} fieldname
+     * @param {string} field_name
      * @returns {string}
      */
-    grabField(obj, fieldname) {
-        if (obj.hasOwnProperty(fieldname) && obj[fieldname]) {
-            return obj[fieldname];
+    grabField(obj, field_name) {
+        if (obj.hasOwnProperty(field_name) && obj[field_name]) {
+            return obj[field_name];
         }
         return '';
     }
@@ -68,7 +131,7 @@ window.FamilyGroup = class FamilyGroup {
             this.grabField(person, 'MiddleName')
         ];
         if (person.LastNameCurrent !== person.LastNameAtBirth) {
-            nm.push(`(${this.grabField(person, 'LastNameAtBirth')})`)
+            nm.push(`(${this.grabField(person, 'LastNameAtBirth')})`);
         }
         const full = nm.concat([
             this.grabField(person, 'LastNameCurrent'),
@@ -77,6 +140,12 @@ window.FamilyGroup = class FamilyGroup {
         return full.join(' ').trim();
     }
 
+    /**
+     * Given a person object, construct a 'short' name
+     *
+     * @param person
+     * @returns {string}
+     */
     shortName(person) {
         const nm = [
             this.grabField(person, 'FirstName'),
@@ -87,41 +156,87 @@ window.FamilyGroup = class FamilyGroup {
 
     /**
      * Although 'maiden' name only really applies to women, it doesn't matter to the code
+     *
+     * @param person
+     * @returns {string}
      */
     maidenName(person) {
         return `${person.FirstName} ${person.LastNameAtBirth}`;
     }
 
+    /**
+     * Create a link to the given persons profile page using the provided linkText
+     *
+     * @param person
+     * @param linkText
+     * @returns {string|*}
+     */
     profileLink(person, linkText) {
         if (person.Id === 0) {
             return linkText;
         }
-        return `<a href='https://www.wikitree.com/wiki/${person.Name}'>${linkText}</a>`;
+        return `<a href='${this.baseWikiURL}/${person.Name}' title>${linkText}</a>`;
     }
 
+    /**
+     * Create a link to the given persons profile page using the full name as link text
+     *
+     * @param person
+     * @returns {string}
+     */
     linkedProfileName(person) {
         return `${this.profileLink(person, this.fullName(person))}${this.makeLinkToSelf(person)}`;
     }
 
+    /**
+     * Create a link to the given persons profile page using the short name as link text
+     * @param person
+     * @returns {string}
+     */
+    linkedShortName(person) {
+        return `${this.profileLink(person, this.shortName(person))}${this.makeLinkToSelf(person)}`;
+    }
+
+    /**
+     * Create a link to the given persons parents profile… parentSide dictated 'Father' or 'Mother'
+     * if requested profile doesn't exist return a link to allow a new profile page to be created
+     *
+     * @param person
+     * @param parentSide
+     * @returns {string}
+     */
     extractParentLink(person, parentSide) {
         if (person[parentSide] && person[parentSide] !== 0) {
             return this.linkedProfileName(person.Parents[person[parentSide]]);
         } else {
-            return `unknown ${parentSide.toLowerCase()}`;
+            return `<span class="BLANK"><a href="${this.baseWikiURL}${this.editFamilyURL}&amp;u=${person.Id}&amp;who=${parentSide.toLowerCase()}">
+                [${parentSide.toLowerCase()}?]</a></span>`;
         }
     }
 
+    /**
+     * Create a link to the given persons profile pic… if no pic is available return a silhouette placeholder
+     * @param person
+     * @returns {string}
+     */
     getProfilePicHTML(person) {
         if (person.Photo) {
-            return `<img alt="Profile image for ${person.RealName}" src="https://www.wikitree.com${person.PhotoData.url}"/><br/>`;
+            return `<img alt="Profile image for ${person.RealName}" src="${this.baseWikiURL}${person.PhotoData.url}"/><br/>`;
         } else {
             if (person.Gender === 'Male') {
-                return `<img alt="No photo available for "${person.RealName}" src="https://www.wikitree.com/images/icons/male.gif.pagespeed.ce.sk2cBn-ts3.gif"/><br/>`;
+                return `<img alt="No photo available for "${person.RealName}" src="${this.baseWikiURL}${this.imgMaleShadow}"/><br/>`;
             }
-            return `<img alt="No photo available for "${person.RealName}" src="https://www.wikitree.com/images/icons/female.gif.pagespeed.ce._HpxLyYvZO.gif"/><br/>`;
+            return `<img alt="No photo available for "${person.RealName}" src="${this.baseWikiURL}${this.imgFemaleShadow}"/><br/>`;
         }
     }
 
+    /**
+     * Create a readable date for the given data and qualifier
+     *
+     * @param aDate
+     * @param qualifier
+     * @returns {string}
+     */
     readableDate(aDate, qualifier) {
         const months = ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
         if (aDate === '0000-00-00' || qualifier === 'blank') {
@@ -146,16 +261,30 @@ window.FamilyGroup = class FamilyGroup {
         return ans;
     }
 
+    /**
+     * Create a link back to this page to switch to the given person—ignore if it is the same person already displayed
+     *
+     * @param person
+     * @returns {string}
+     */
     makeLinkToSelf(person) {
         if (person.Id !== this.person_id) {
             let html = ` <a href="#name=${person.Name}&view=familygroup">`;
-            html += `<img src="https://www.wikitree.com/images/icons/family-group.gif.pagespeed.ce.xRjS57QXMl.gif" border="0" width="7" height="11" alt="family group sheet" title="Family Group Sheet">`;
+            html += `<img src="${this.baseWikiURL}${this.imgFamilyGroup}" border="0" width="7" height="11" 
+                    alt="family group sheet" title="Family Group Sheet">`;
             html += `</a>`;
             return html;
         }
         return '';
     }
 
+    /**
+     * Create the mini-bio for a single person, set showPic to 'false' to suppress any picture
+     *
+     * @param person
+     * @param showPic
+     * @returns {string}
+     */
     createMiniBioHTML(person, showPic = true) {
         let html = `<td class="gender_${person.Gender}">`;
         if (showPic) {
@@ -172,6 +301,16 @@ window.FamilyGroup = class FamilyGroup {
         return html;
     }
 
+    /**
+     * Create a single family group with the given person plus spouse (spousal_relation indicates 'mother' or 'father')
+     * 'spouse' may be 'null' to indicate no spouse
+     *
+     * @param person
+     * @param spouse
+     * @param spousal_relation
+     * @param spousesKeyStr
+     * @returns {string}
+     */
     extractFamilyGroupHTML(person, spouse, spousal_relation, spousesKeyStr) {
         // We're passed a string, make it an integer!
         let spousesKey = parseInt(spousesKeyStr);
@@ -206,6 +345,25 @@ window.FamilyGroup = class FamilyGroup {
         return html;
     }
 
+    /**
+     * Create a link to the given persons 'Tree and Tools' page using linkText
+     *
+     * @param person
+     * @param linkText
+     * @returns {string}
+     */
+    linkToTreeAndTools(person, linkText) {
+        let person_parts = person.Name.split('-');
+        let url = `${this.baseWikiURL}${this.urlTreeAndTools}${person_parts[0]}-Family-Tree-${person_parts[1]}`;
+        return `<a href="${url}">${linkText}</a>`;
+    }
+
+
+    /**
+     * Set the text within the Description box to reflect the chosen person
+     *
+     * @param person
+     */
     setViewDescription(person) {
         let html = `Here is a family group view for illustrating the marriage`;
         if (person.Spouses && Object.keys(person.Spouses).length > 1) {
@@ -219,17 +377,46 @@ window.FamilyGroup = class FamilyGroup {
 
         let fatherName = this.extractParentLink(person, 'Father');
         let motherName = this.extractParentLink(person, 'Mother');
+        let childType = (person.Gender === 'Male') ? 'son' : 'daughter';
 
-        html += ` the son of ${fatherName} and ${motherName}. 
-            See ${person.RealName}'s Tree & Tools page for more views.`;
+        html += ` the ${childType} of ${fatherName} and ${motherName}. 
+            See ${this.linkedShortName(person)}'s ${this.linkToTreeAndTools(person, 'Tree &amp; Tools page')} for more views.`;
         $("#view-description").html(html);
     }
 
+    /**
+     * Create an introduction line for the list of children
+     *
+     * @param person
+     * @returns {string}
+     */
+    createChildListIntroductionLine(person) {
+        // Select the traditional pronoun based upon declared gender
+        let pronoun = 'his';
+        if (person.Gender !== 'Male') {
+            pronoun = 'her';
+        }
+        // Account for single or multiple spouses
+        let relation = 'spouse is';
+        if (person.Spouses && Object.keys(person.Spouses).length > 1) {
+            relation = 'spouses are';
+        }
+        // Build the introduction line
+        return `<h2>Children of ${person.RealName}</h2>
+                <p>The above shows children where ${this.profileLink(person, person.RealName)}'s ${relation} the 
+                other parent and may therefore be incomplete. Here is a simple list of all ${pronoun} children:</p>
+                <ol>`;
+    }
 
+    /**
+     * Here is the donkey work: control the requesting of data and then marshall the processing into the web page
+     *
+     * @returns {Promise<void>}
+     */
     async displayFamilyGroup() {
         // Attempt to retrieve the data for the given person id
         let data = await WikiTreeAPI.postToAPI({action: "getPerson", key: this.person_id, fields: this.profileFields});
-        // It's an error if nothing is returned
+        // It is an error if nothing is returned
         if (data.length !== 1) {
             $(this.selector).html(`Error retrieving information for person id ${this.person_id}`);
             return;
@@ -241,13 +428,14 @@ window.FamilyGroup = class FamilyGroup {
         // gauge if it is a private profile that the logged-in user has access to
         if (person.Privacy < 50 && !person.Gender) {
             $(this.selector).html(
-                `<p>Sorry, this profile is <a href="/wiki/Privacy">Private</a> and you are not on the profile's 
-                <a href="/wiki/Trusted_List">Trusted List</a>.</p>`
+                `<p>Sorry, this profile is <a href="${this.baseWikiURL}${this.urlPrivacy}">Private</a> 
+                and you are not on the profile's 
+                <a href="${this.baseWikiURL}${this.urlTrustedList}">Trusted List</a>.</p>`
             );
             return;
         }
 
-        // We have all the necessary information, so it's time to clear out our tree view and start filling it.
+        // We have all the necessary information, so it is time to clear out our tree view and start filling it.
         // We'll use two main divs the upper one holding the family group(s) for any multiple marriages, and the
         // lower one just a list of all children across all marriages
         $(this.selector).html(`<div id="family_group"></div><div id="children_list"></div>`);
@@ -257,39 +445,8 @@ window.FamilyGroup = class FamilyGroup {
 
         const wv = $('#family_group');
 
-        // wv.html(`Family Group Sheet for ${person.Name}`);
-
-        // DEBUG routine for following section
-        function toString(obj) {
-            if (obj) {
-                const s = Object.entries(obj).map(([k, v]) => `${k}: ${v}`).join('<br/> ');
-                return `<p style="font-family: 'Courier New',monospace">${s}</p>`;
-            }
-            return '';
-        }
-
         if (0) {
-            // let toString = oToStr();
-
-            wv.append(`<p>Person</p>${toString(person)}`);
-            wv.append(`<p>Data Status</p>${toString(person.DataStatus)}`);
-            wv.append(`<p>Photo Data</p>${toString(person.PhotoData)}`);
-            wv.append(`<p>Parents</p>${toString(person.Parents)}`);
-            wv.append(`<p>Children</p>${toString(person.Children)}`);
-            wv.append(`<p>Siblings</p>${toString(person.Siblings)}`);
-            if (person.Spouses && Object.keys(person.Spouses).length > 0) {
-                let spHTML = '';
-                let sp = 0;
-                for (const spousesKey in person.Spouses) {
-                    if (person.Spouses.hasOwnProperty(spousesKey)) {
-                        const spouse = person.Spouses[spousesKey];
-                        sp += 1;
-                        spHTML += `<p>Spouse #${sp}</p>${toString(spouse)}`;
-                        spHTML += `<p>Spouse #${sp} Data Status</p>${toString(spouse.data_status)}`;
-                    }
-                }
-                wv.append(`<p>Spouses</p>${spHTML}`);
-            }
+            this.showDEBUG(wv, person);
         }
 
         // Check which relationship is changing to work out which child begins with which
@@ -317,7 +474,6 @@ window.FamilyGroup = class FamilyGroup {
                 Date: x.marriage_date
             })));
 
-            // for (const spousesKey in person.Spouses) {
             for (const spouseEntry in spouseList) {
                 if (spouseList.hasOwnProperty(spouseEntry)) {
                     const spousesKey = spouseList[spouseEntry].Id;
@@ -331,7 +487,7 @@ window.FamilyGroup = class FamilyGroup {
                         wv.append(`<h3>${person.RealName} married ${this.maidenName(spouse)}, 
                             ${this.readableDate(spouse.marriage_date, spouse.data_status.marriage_date)}
                             ${marr_place}</h3>`);
-                        // wv.append(`<p>${toString(spouse)}</p>`);
+
                         let html = this.extractFamilyGroupHTML(person, spouse, spousal_relation, spousesKey);
                         wv.append(html);
                     }
@@ -343,18 +499,7 @@ window.FamilyGroup = class FamilyGroup {
         }
 
         if (person.Children && Object.keys(person.Children).length > 0) {
-            let pronoun = 'his';
-            if (person.Gender !== 'Male') {
-                pronoun = 'her';
-            }
-            let relation = 'spouse is';
-            if (person.Spouses && Object.keys(person.Spouses).length > 1) {
-                relation = 'spouses are';
-            }
-            let html = `<h2>Children of ${person.RealName}</h2>
-                <p>The above shows children where ${this.profileLink(person, person.RealName)}'s ${relation} the 
-                other parent and may therefore be incomplete. Here is a simple list of all ${pronoun} children:</p>
-                <ol>`;
+            let html = this.createChildListIntroductionLine(person);
             for (const childListKey in person.childList) {
                 if (person.childList.hasOwnProperty(childListKey)) {
                     const childrenKey = person.childList[childListKey].Id;
@@ -366,6 +511,43 @@ window.FamilyGroup = class FamilyGroup {
             }
             html += `</ol>`;
             $('#children_list').append(html);
+        }
+    }
+
+    /**
+     * Push DEBUG details to the webpage
+     *
+     * @param wv
+     * @param person
+     */
+    showDEBUG(wv, person) {
+        // DEBUG routine for following section
+        function mkStr(obj) {
+            if (obj) {
+                const s = Object.entries(obj).map(([k, v]) => `${k}: ${v}`).join('<br/> ');
+                return `<p style="font-family: 'Courier New',monospace">${s}</p>`;
+            }
+            return '';
+        }
+
+        wv.append(`<p>Person</p>${mkStr(person)}`);
+        wv.append(`<p>Data Status</p>${mkStr(person.DataStatus)}`);
+        wv.append(`<p>Photo Data</p>${mkStr(person.PhotoData)}`);
+        wv.append(`<p>Parents</p>${mkStr(person.Parents)}`);
+        wv.append(`<p>Children</p>${mkStr(person.Children)}`);
+        wv.append(`<p>Siblings</p>${mkStr(person.Siblings)}`);
+        if (person.Spouses && Object.keys(person.Spouses).length > 0) {
+            let spHTML = '';
+            let sp = 0;
+            for (const spousesKey in person.Spouses) {
+                if (person.Spouses.hasOwnProperty(spousesKey)) {
+                    const spouse = person.Spouses[spousesKey];
+                    sp += 1;
+                    spHTML += `<p>Spouse #${sp}</p>${mkStr(spouse)}`;
+                    spHTML += `<p>Spouse #${sp} Data Status</p>${mkStr(spouse.data_status)}`;
+                }
+            }
+            wv.append(`<p>Spouses</p>${spHTML}`);
         }
     }
 };
