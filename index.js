9f075b0d97eff764cc611f410bd781c403ba9f64
udjeni <107791945+udjeni@users.noreply.github.com>
2022-11-01 10:17:47 +0200

    Add a couples dynamic tree view

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
         "familygroup": new FamilyView(),
         "printer-friendly": new PrinterFriendlyView(WikiTreeAPI, 5),
         "calendar": new calendarView(),
+        "couples": new CouplesTreeView(),
         "nameTest": new NameTestView(),
     };
 
