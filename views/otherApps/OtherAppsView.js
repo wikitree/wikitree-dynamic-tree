b0be3df53ae125f7cbb969551b6c19a291e7d323
Brian Casey <brian@rootlevelservices.com>
2022-11-14 17:57:27 +0000

    Change Other Apps description to link to a tree-widget page.

--- views/otherApps/OtherAppsView.js
+++ views/otherApps/OtherAppsView.js
@@ -2,12 +2,32 @@ window.OtherAppsView = class OtherAppsView extends View {
     meta() {
         return {
             title: "Other Apps",
-            description: `Check out other tools created by independent developers in the WikiTree Community: <a href="https://www.wikitree.com/wiki/Help:Apps">Help:Apps</a>.`,
+            description: `Check out other tools created by independent developers in the WikiTree Community.`,
             docs: "",
         };
     }
 
     init(containerSelector, personID) {
         //document.location = "https://www.wikitree.com/wiki/Help:Apps";
+        this.loadDescription(personID);
+    }
+    
+    async loadDescription(personID) {
+        let data = await WikiTreeAPI.postToAPI({ action: "getPerson", key: personID, fields: 'Id,Name' });
+        if (data.length != 1) {
+            wtViewRegistry.showError(`There was an error starting with ${personID}.`);
+            wtViewRegistry.hideInfoPanel();
+            return;
+        }
+
+        let p = data[0].person;
+        if (!p?.Name) {
+            let err = `The starting profile data could not be retrieved.`;
+            wtViewRegistry.showError(err);
+            wtViewRegistry.hideInfoPanel();
+            return;
+        }
+
+        wtViewRegistry.setDescription(`Check out other tools created by independent developers in the WikiTree Community: <a href="https://www.wikitree.com/treewidget/${data[0].person.Name}/77">Tree Widget #77</a>`);
     }
 };
