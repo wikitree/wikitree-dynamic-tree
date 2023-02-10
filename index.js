f020ebba305f96a673776511705a84edb0ed1e69
Brian Casey <brian@rootlevelservices.com>
2023-02-10 00:36:33 +0000

    Enable checking of optional global, requireAppsLogin. If it exists and is set, then instead of just displaying a login button, click it to require the user go to the API clientLogin page to log in. This has no impact if the requireAppsLogin parameter is unset or false.

--- index.js
+++ index.js
@@ -15,21 +15,27 @@ window.addEventListener("DOMContentLoaded", (event) => {
         WikiTreeAPI,
         (events = {
             onLoggedIn: (user) => {
-                document.querySelector("#wt-api-login").innerHTML = `Logged into Apps: ${user.name} (<a class="apiLogout" href="#">Logout</a>)`;
+                document.querySelector(
+                    "#wt-api-login"
+                ).innerHTML = `Logged into Apps: ${user.name} (<a class="apiLogout" href="#">Logout</a>)`;
             },
             onUnlogged: () => {
                 document.querySelector("#wt-api-login").innerHTML = `
-          <form action="https://api.wikitree.com/api.php" method="POST">
+          <form id="appsLoginForm" action="https://api.wikitree.com/api.php" method="POST">
               <input type="hidden" name="action" value="clientLogin">
               <input type="hidden" id="returnURL" name="returnURL" value="${window.location.href}">
               <input type="submit" class="small" value="Apps Login"
                 title="Please login to the WikiTree Apps to use the Tree Viewer on non-public profiles.">
           </form>
           `;
+
+                if (typeof requireAppsLogin != "undefined" && requireAppsLogin) {
+                    $("#appsLoginForm").submit();
+                }
             },
         })
     );
-    $('body').on('click', '.apiLogout', function(e) {
+    $("body").on("click", ".apiLogout", function (e) {
         e.preventDefault();
         loginManager.logout();
     });
