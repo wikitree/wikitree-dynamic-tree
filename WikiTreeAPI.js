8f1a1d313691d86bc81afb97f4d58f9e8c6bd100
Clarke-11007 <114438918+Clarke-11007@users.noreply.github.com>
2023-06-17 15:27:47 -0400

    commented out some console.logs left in by mistake

--- WikiTreeAPI.js
+++ WikiTreeAPI.js
@@ -402,7 +402,7 @@ WikiTreeAPI.getRelatives = async function (appId, IDs, fields, options = {}) {
  */
 WikiTreeAPI.getPeople = async function (appId, IDs, fields, options = {}) {
     let theKeys = IDs;
-    console.log("IDs", IDs, typeof IDs);
+    // console.log("IDs", IDs, typeof IDs);
     if (typeof IDs == "object" /*  && IDs.indexOf(",") > -1 */) {
         theKeys = IDs.join(",");
     }
@@ -421,7 +421,7 @@ WikiTreeAPI.getPeople = async function (appId, IDs, fields, options = {}) {
             getPeopleParameters[key] = element;
         }
     }
-    console.log("NEED: getPeopleParameters: ", getPeopleParameters);
+    // console.log("NEED: getPeopleParameters: ", getPeopleParameters);
 
     const result = await WikiTreeAPI.postToAPI(getPeopleParameters);
     return result[0].people;
@@ -492,7 +492,7 @@ WikiTreeAPI.postToAPI = async function (postData) {
 
     const response = await fetch(API_URL, options);
     if (!response.ok) {
-        console.log(" ${response.status}: ${response.statusText} ");
+        // console.log(" ${response.status}: ${response.statusText} ");
         throw new Error(`HTTP error! Status: ${response.status}: ${response.statusText}`);
     }
     return await response.json();
